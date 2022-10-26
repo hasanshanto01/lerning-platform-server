5e77a08e160a59b6ca8c3c09ca67e714dcedf7ec
hasanshanto01 <hasanshanto01@gmail.com>
2022-10-27 01:00:43 +0600

    course id api created

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ app.use(cors());
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('Learning Platform server running');
+    res.send('Learning server running...');
 });
 
+// all course
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+// single course
+app.get('/courses/:id', (req, res) => {
+    // console.log(typeof req.params.id);
+    const id = parseInt(req.params.id);
+    const course = courses.find(c => c.id === id);
+    res.send(course);
+})
+
 app.listen(port, () => {
-    console.log('Learning Platform server running on port:', port);
+    console.log('Learning server running on port:', port);
 });
